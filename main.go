18a653a6fa201f9e8ce7f916006b4ca14aed021f
Aris Husanu <abane94@gmail.com>
2018-04-09 16:31:25 -0500

    adds post endpoint

--- main.go
+++ main.go
@@ -35,7 +35,30 @@ import (
 	"net/http"
 	"log"
 	"os"
+	"encoding/json"
+	"io/ioutil"
 )
+
+type message struct {
+	group_id string `json:"group_id"`
+	name string `json:"name"`
+	text string `json:"text"`
+}
+
+// {
+// 	"attachments": [],
+// 	"avatar_url": "https://i.groupme.com/123456789",
+// 	"created_at": 1302623328,
+// 	"group_id": "1234567890",
+// 	"id": "1234567890",
+// 	"name": "John",
+// 	"sender_id": "12345",
+// 	"sender_type": "user",
+// 	"source_guid": "GUID",
+// 	"system": false,
+// 	"text": "Hello world ☃☃",
+// 	"user_id": "1234567890"
+//   }
 
 
 func main() {
@@ -47,6 +70,26 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Team 2 says:<br/>Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.HandleFunc("/group-me/msg", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(body))
+		
+		msg := message{}
+		err = json.Unmarshal(body, &msg)
+		if err != nil {
+			// fmt.Println(err)
+			log.Println(err)
+			return
+		}
+		log.Println(r.Body)
+		// fmt.Fprintf(w, "Team 2 says:\nHello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, msg.text)
 	})
 
 	http.ListenAndServe(":" + port, nil)
