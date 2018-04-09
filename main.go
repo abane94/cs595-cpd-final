b7314a585381e752c13bb791f9e7c1ce1cb72622
Aris Husanu <abane94@gmail.com>
2018-04-09 10:25:03 -0500

    changed to hello world

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
+
+// 	"github.com/gin-gonic/gin"
+// 	_ "github.com/heroku/x/hmetrics/onload"
+// )
+
+// func main() {
+// 	port := os.Getenv("PORT")
+
+// 	if port == "" {
+// 		log.Fatal("$PORT must be set")
+// 	}
+
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	router.LoadHTMLGlob("templates/*.tmpl.html")
+// 	router.Static("/static", "static")
+
+// 	router.GET("/", func(c *gin.Context) {
+// 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+// 	})
+
+// 	router.Run(":" + port)
+// }
+
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"log"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
+
 
 func main() {
 	port := os.Getenv("PORT")
@@ -16,14 +45,9 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Team 2 says:<br/>Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	router.Run(":" + port)
+	http.ListenAndServe(":" + port, nil)
 }
