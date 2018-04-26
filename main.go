c0bfdcf8331fc63b42f493b6ae0e00fc4417943a
Shawn <shawnpstrong@gmail.com>
2018-04-26 15:47:14 -0500

    yandex json response still isn't unmarshalled correctly

--- main.go
+++ main.go
@@ -132,7 +132,9 @@ func main() {
 			// resp1, err1 := http.Get("https://translate.yandex.net/api/v1.5/tr.json/translate?" +
 			// 	"key=trnsl.1.1.20180404T152827Z.de1604f76f1d895c.8909d7acdac0907096f9a3cac7ecd33db02e0650&lang=en-de" +
 			// 	"&text= " + msgToTranslate)
-			url := "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180404T152827Z.de1604f76f1d895c.8909d7acdac0907096f9a3cac7ecd33db02e0650&lang=en-de&text= " + msgToTranslate
+			url := "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+				"key=trnsl.1.1.20180404T152827Z.de1604f76f1d895c.8909d7acdac0907096f9a3cac7ecd33db02e0650&lang=en-de" +
+				"&text= " + msgToTranslate
 			client := http.Client{
 				Timeout: time.Second * 2, // Maximum of 2 secs
 			}
