ecea0782ea78c97d1f2a62e3721616f918dbc2f9
Shawn <shawnpstrong@gmail.com>
2018-04-26 12:04:58 -0500

    testing with translation service

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const BOT_ID = "c947489260df70ef469fc34ea8" // need to get once its registered with groupMe
@@ -115,7 +116,6 @@ func main() {
 		var msg groupMe_message_post
 		err = json.Unmarshal(body, &msg) // parses the json from the body
 		if err != nil {
-			// fmt.Println(err)
 			log.Println(err)
 			return
 		}
@@ -124,29 +124,50 @@ func main() {
 		log.Println("LOG: msg.text :")
 		log.Println(msg.Text)
 
-		resp1, err1 := http.Post("https://api.groupme.com/v3/bots/post?bot_id="+BOT_ID+"&text=Hello+world", "", nil)
-		if err1 != nil {
-			// handle err
+		if strings.HasPrefix(msg.Text, "translate: ") {
+			var msgToTranslate string = msg.Text[11:len(msg.Text)]
+			resp1, err1 := http.Post("https://translate.yandex.net/api/v1.5/tr.json/translate?"+
+				"key=trnsl.1.1.20180404T152827Z.de1604f76f1d895c.8909d7acdac0907096f9a3cac7ecd33db02e0650&lang=en-de"+
+				"&text= "+msgToTranslate, "", nil)
+			if err1 != nil {
+				log.Fatal(err1)
+				return
+			} else {
+				// need to call this right after checking err else resource leak
+				defer resp1.Body.Close()
+				var translatedMsg translated_respone
+				err = json.Unmarshal(body, &translatedMsg)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				var translatedMsgArr []string = translatedMsg.Text
+				resp2, err2 := http.Post("https://api.groupme.com/v3/bots/post?bot_id="+BOT_ID+"&text="+translatedMsgArr[0], "", nil)
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+				defer resp2.Body.Close()
+			}
+
 		}
-		defer resp1.Body.Close()
 
 		// try 2 for getting the bot to talk
-		groupMeMsg.text = "Hello!"
-		payloadBytes, err := json.Marshal(groupMeMsg)
-		if err != nil {
-			// handle err
-		}
-		body2 := bytes.NewReader(payloadBytes)
-		req, err := http.NewRequest("POST", "https://api.groupme.com/v3/bots/post", body2)
-		if err != nil {
-			// handle err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			// handle err
-		}
-		defer resp.Body.Close()
+		// groupMeMsg.text = "Hello!"
+		// payloadBytes, err := json.Marshal(groupMeMsg)
+		// if err != nil {
+		// 	// handle err
+		// }
+		// body2 := bytes.NewReader(payloadBytes)
+		// req, err := http.NewRequest("POST", "https://api.groupme.com/v3/bots/post", body2)
+		// if err != nil {
+		// 	// handle err
+		// }
+		// req.Header.Set("Content-Type", "application/json")
+		// resp, err := http.DefaultClient.Do(req)
+		// if err != nil {
+		// 	// handle err
+		// }
+		// defer resp.Body.Close()
 		// sends response, not sure how groupMe will handle this
 		//fmt.Fprintf(w, msg.Text)
 	})
